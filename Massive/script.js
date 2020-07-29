9bff8a5be6ce3df67c2a76158029db9a0f1ba314
Stanislav <skkc@yandex.ru>
2020-07-29 09:51:35 +0300

    litlle bit changes

--- Massive/script.js
+++ Massive/script.js
@@ -41,12 +41,12 @@
 // console.log(`Номер максимального эксперимента равен: ${maxNumArr}`);
 
 
-const scores = [60, 50, 60, 58, 54, 54,
+const scores = [60, 50, 60, 58, 54,
     58, 50, 52, 54, 48, 69,
     34, 55, 51, 52, 44, 51,
-    69, 64, 66, 55, 52, 61, 205,
-    46, 31, 57, 52, 44, 18, 23, 43, 31, 24, 65, 342,
-    41, 53, 55, 61, 51, 44, 108
+    69, 64, 66, 55, 52, 61,
+    46, 31, 57, 52, 44, 18, 23,
+    41, 53, 55, 61, 51, 44
 ];
 // let highScore = 0;
 // let highScoreNumber = [];
@@ -73,7 +73,24 @@ const scores = [60, 50, 60, 58, 54, 54,
 // (
 // `Max result: номер с макс.значением ${numberHighSort.indexOf(numberHighSort[0])}: ${numberHighSort[0]}}`
 // );
+const costs = [0.25, 0.27, 0.25, 0.25, 0.25, 0.25,
+    0.33, 0.31, 0.25, 0.29, 0.27, 0.22,
+    0.31, 0.25, 0.25, 0.33, 0.21, 0.25,
+    0.25, 0.25, 0.28, 0.25, 0.24, 0.22,
+    0.20, 0.25, 0.30, 0.25, 0.24, 0.25,
+    0.25, 0.25, 0.27, 0.25, 0.26, 0.29];
 
+function getCosts(scores, costs, highScore) {
+    let cost = 100;
+    let index;
+    for(let i = 0; i < scores.length; i++) {
+        if(scores[i] == highScore && cost > costs[i]) {
+             index = i;
+             cost = costs[i];
+        }
+    } 
+    return index;
+}
 
 function scoresRealise(scores) {
     let highScore = 0;
@@ -104,9 +121,13 @@ function getBest(scores, highScore) {
 
 const highScore = scoresRealise(scores);
 const bestSolution = getBest(scores, highScore);
+const mostCost = getCosts(scores, costs, highScore);
 console.log(`Количество результатов: ${scores.length}`);
 console.log(`Максимальный результат равен: ${highScore}`);
 console.log(`Номер максимального значения равен: ${bestSolution}`);
+console.log(`Макс. эффективный результат равен: ${mostCost}`);
+
+
 
 
 
